d0041e5fd62b8e86df1914a1bc1c707c07963987
golneshin <59548304+golneshin@users.noreply.github.com>
2024-06-18 14:58:32 +0330

    new commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomePage from './pages/HomePage'
 
 const App = () => {
   return (
-    <h1>
-      Welcome to Proshop
-    </h1>
+    <>
+      <Header/>
+      <main className='py-3'>
+        <Container>
+          <HomePage/>
+        </Container>
+      </main>
+      <Footer/>
+    </>
   )
 }
 
